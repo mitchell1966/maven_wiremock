067b651aeda15db9d1daf5c2f5f9547e06df2902
Mitchell Agoma <mitchell.agoma@digital.hmrc.gov.uk>
2017-05-02 17:07:38 +0100

    Changes to runner and pom file

--- src/test/java/runner/AcceptanceTests.java
+++ src/test/java/runner/AcceptanceTests.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features={"specs/"},glue ={"step_def"})
+@CucumberOptions(features={"src/test/specs/"},glue ={"step_def"})
 public class AcceptanceTests {
 
 }
